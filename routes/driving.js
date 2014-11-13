2d1a3141d4f303445831f9441e1bf55df30b2d3a
Daniel <pettenon.daniel@gmail.com>
2014-11-13 11:29:46 +0100

    drive setup

--- routes/driving.js
+++ routes/driving.js
@@ -52,15 +52,38 @@ router.get('/go', function(req, res) {
 
 router.get('/turn', function(req, res) {
   if(req.query.direction == 1) {
-    setGpio(11, 1);
-    res.json({message: 'TURN RIGHT!', time: Date.now()});
+    setGpio(11, 1, function(err) {
+      if(err)
+        res.json({message: 'CAN\'T TURN RIGHT!', time: Date.now(), error: err});
+      else
+        res.json({message: 'TURN RIGHT!', time: Date.now()});
+
+    });
   } else if(req.query.direction == -1) {
-    setGpio(15, 1);
-    res.json({message: 'TURN LEFT!', time: Date.now()});
+    setGpio(15, 1, function(err) {
+      if(err)
+        res.json({message: 'CAN\'T TURN LEFT!', time: Date.now(), error: err});
+      else
+        res.json({message: 'TURN LEFT!', time: Date.now()});
+
+    });
   } else {
     setGpio(15, 0);
     setGpio(13, 0);
     res.json({message: 'DON\'T TURN!', time: Date.now()});
+
+    setGpio(15, 0, function(err1) {
+      if(err1) {
+        res.json({message: 'CAN\'T TURN!', time: Date.now(), error: err});
+      } else {
+        setGpio(13, 0, function(err2) {
+          if(err2)
+            res.json({message: 'CAN\'T STOP TURNING!', time: Date.now(), error: err});
+          else
+           res.json({message: 'STOP TURNING!', time: Date.now()});
+        });
+      }
+    });
   }
 });
 
